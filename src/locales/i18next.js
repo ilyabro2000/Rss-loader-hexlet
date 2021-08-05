5849155362d542d1eb7ee6f1f698e81b9b51c291
ilya <alieff.ilya@gmail.com>
2021-08-05 16:56:27 +0300

    fix i18next config

--- src/locales/i18next.js
+++ src/locales/i18next.js
@@ -1,7 +1,5 @@
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import ru from './ru';
-import en from './en';
 
 const initObj = {
   fallbackLng: 'ru',
@@ -9,8 +7,48 @@ const initObj = {
   whitelist: ['en', 'ru'],
   debug: false,
   resources: {
-    ru,
-    en,
+    ru: {
+      buttons: {
+        preview: 'Просмотр',
+        modalWindow: {
+          close: 'Закрыть',
+          readMore: 'Читать Полностью',
+        },
+      },
+      feeds: 'Фиды',
+      posts: 'Посты',
+      waiting: 'Ожидание ответа...',
+      errors: {
+        url: 'Ссылка должна быть валидным URL',
+        doubleUrl: 'RSS уже существует',
+        network: 'Ошибка сети',
+        unknown: 'Неизвестная ошибка',
+        parser: 'Ресурс не содержит валидный RSS',
+      },
+      loadSuccess: 'RSS успешно загружен',
+    },
+    en: {
+      translation: {
+        buttons: {
+          preview: 'View',
+          modalWindow: {
+            close: 'Close',
+            readMore: 'Read',
+          },
+        },
+        feeds: 'Feeds',
+        posts: 'Posts',
+        waiting: 'Wait for response...',
+        errors: {
+          url: 'Must be valid url',
+          doubleUrl: 'Rss already exists',
+          network: 'Network Error',
+          unknown: 'Unknown error',
+          parser: 'This source does not contain valid rss',
+        },
+        loadSuccess: 'Rss has been loaded',
+      },
+    },
   },
 };
 
