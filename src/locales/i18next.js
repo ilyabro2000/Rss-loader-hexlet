6a9e5966baa536b3ac5854d62079decfa8d7e08f
ilya <alieff.ilya@gmail.com>
2021-08-05 17:59:10 +0300

    fix locales

--- src/locales/i18next.js
+++ src/locales/i18next.js
@@ -1,5 +1,6 @@
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import ru from './ru.json';
 
 const initObj = {
   fallbackLng: 'ru',
@@ -7,48 +8,7 @@ const initObj = {
   whitelist: ['en', 'ru'],
   debug: false,
   resources: {
-    ru: {
-      buttons: {
-        preview: 'Просмотр',
-        modalWindow: {
-          close: 'Закрыть',
-          readMore: 'Читать Полностью',
-        },
-      },
-      feeds: 'Фиды',
-      posts: 'Посты',
-      waiting: 'Ожидание ответа...',
-      errors: {
-        url: 'Ссылка должна быть валидным URL',
-        doubleUrl: 'RSS уже существует',
-        network: 'Ошибка сети',
-        unknown: 'Неизвестная ошибка',
-        parser: 'Ресурс не содержит валидный RSS',
-      },
-      loadSuccess: 'RSS успешно загружен',
-    },
-    en: {
-      translation: {
-        buttons: {
-          preview: 'View',
-          modalWindow: {
-            close: 'Close',
-            readMore: 'Read',
-          },
-        },
-        feeds: 'Feeds',
-        posts: 'Posts',
-        waiting: 'Wait for response...',
-        errors: {
-          url: 'Must be valid url',
-          doubleUrl: 'Rss already exists',
-          network: 'Network Error',
-          unknown: 'Unknown error',
-          parser: 'This source does not contain valid rss',
-        },
-        loadSuccess: 'Rss has been loaded',
-      },
-    },
+    ru,
   },
 };
 
