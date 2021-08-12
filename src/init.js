4f811c77161ebb6234a1d50d9a3e93e56eedbae0
ilya <alieff.ilya@gmail.com>
2021-08-12 16:51:05 +0300

    refactoring

--- src/init.js
+++ src/init.js
@@ -4,5 +4,6 @@ import app from './app.js';
 
 export default () => {
   const i18nextInstance = i18next.createInstance();
-  i18nextInstance.init(options).then(() => app(i18nextInstance));
+  i18nextInstance.init(options);
+  app(i18nextInstance);
 };
